97252459efd008a5bbb26f6a49cfef78193c16a0
Matt-Bromley <bromless1@gmail.com>
2020-02-24 13:25:45 -0500

    finishing design of login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,29 +1,36 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonLabel, IonGrid, IonRow, IonButton } from "@ionic/react";
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonLabel, IonGrid, IonRow, IonButton, IonIcon } from "@ionic/react";
 import React from "react";
 import topImage from "../assets/loginPageTop.png";
-import bottomImage from "../assets/loginPageBottom.png"
+import bottomImage from "../assets/loginPageBottom.png";
+import googleLogin from "../assets/googleLogin.png";
 import "../styles/Login.scss";
 
 const Login: React.FC = () => {
     return (
       <React.Fragment>        
-        
+
         <IonContent class="ion-padding">
           <div className="div-content">
             <h1>Login</h1>
 
           <div className="field">
             <IonItem className="input">
-              <IonInput placeholder="Email" required></IonInput>
+              <IonInput placeholder="Email or Username" required></IonInput>
             </IonItem>
             <IonItem className="input">
-              <IonInput placeholder="Password" required></IonInput>
+              <IonInput placeholder="Password" type="password" required></IonInput>
             </IonItem>
           </div>
 
-            <p>Forgot Password?</p>
+            <p className="link-text">Forgot Password?</p>
 
             <IonButton className="button">Login</IonButton>
+
+            <div className="alt-login">
+              <img src={googleLogin} />
+
+              <p>Don't have an account?<br /><span className="link-text">Sign Up</span></p>
+            </div>
           </div>
         </IonContent>
         <img id="top-border" src={topImage} />
