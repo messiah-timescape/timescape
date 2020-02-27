1b9c2bf50b17ac6c25c49f453882748d96c0e9b7
Nathan <nathan.chan.zh@gmail.com>
2020-02-26 21:24:00 -0500

    added ios and android folders

---
+++ android/app/src/main/assets/public/static/js/5.433c75f0.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptimescape=this.webpackJsonptimescape||[]).push([[5],{359:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(40),o=(e(11),function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.g)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(i.m)((function(){return c.scrollToTop(300)}))}))}}))}))})}}]);
+//# sourceMappingURL=5.433c75f0.chunk.js.map
