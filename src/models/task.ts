2ec8d6935a841de751cdd6c308b7c85b216175f0
Leanne <lw1298@messiah.edu>
2020-03-05 13:01:06 -0500

    I had to remove dummy data from the task model used for the test

--- src/models/task.ts
+++ src/models/task.ts
@@ -4,13 +4,12 @@ import BaseModel from "./base_model";
 import { Collection } from "fireorm";
 
 export class Task extends BaseModel<Task> {
-    order: number = 1;
-    name: string = "Complete Test";
-    notes: string | null = "I'm trying to test my Task class as a SubCollection of User";
-    deadline: Date = moment().toDate(); // I think Firestore allows you to use @ServerTimestamp 
-    times: Array<Moment> | null = null;
-    tag_list: Tag | null = null;
-    Task: Task | null = null;
+    order!: number;
+    name!: string;
+    notes!: string | null;
+    deadline!: Date; // I think Firestore allows you to use @ServerTimestamp 
+    times!: Array<Moment> | null;
+    tag_list!: Tag | null;
+    Task!: Task | null;
     completed: boolean = true;
 }
-
