77fbd595b07e48e059a3d3d69720def07584f5f4
Nathan <nathan.chan.zh@gmail.com>
2020-03-05 13:03:45 -0500

    Changed google oauth return to indicate whether user is new

--- src/controllers/user/login.ts
+++ src/controllers/user/login.ts
@@ -32,21 +32,20 @@ export let userlogin_google_oauth = () => {
     .auth()
     .signInWithPopup(provider)
     .then(async result => {
-      console.log('HERE-1');
       if (
         result &&
         result.credential &&
         result.user
       ) {
-        console.log('HERE-2');
         let token = (result.credential as firebase.auth.OAuthCredential).accessToken;
         let user = FirebaseUser.create_from_firebase(result.user);
         user.google_access_token = token;
-        await user.user().then(user => {
-          console.log(user);
-          CurrentUser.set_user(user);
-        });
-        return user;
+        let create_user_status = await user.create_or_load_user();
+        await CurrentUser.set_user(create_user_status.user);
+        return {
+          user: user,
+          new: create_user_status.new
+        };
       } else {
         throw new Error("User not logged in");
       }
