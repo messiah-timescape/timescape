874cdc26a09cc446c99f15266e84995001e6fce6
Nathan <nathan.chan.zh@gmail.com>
2020-03-05 13:22:01 -0500

    Fixed task model

--- src/models/task.ts
+++ src/models/task.ts
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
-import moment, { Moment, now } from "moment";
+import { Moment} from "moment";
 import { Tag } from ".";
 import BaseModel from "./base_model";
-import { Collection } from "fireorm";
 
 export class Task extends BaseModel<Task> {
     order!: number;
@@ -13,11 +11,4 @@ export class Task extends BaseModel<Task> {
     tag_list!: Tag | null;
     Task!: Task | null;
     completed: boolean = true;
-=======
-export class Task {
-    order!: number;
-    name!: string;
-    notes!: string;
-    deadline!: Date;   
->>>>>>> 14fde4dcd67839c52d1fab60a342b911ae7f5400
 }
