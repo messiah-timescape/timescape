089a4add94cc184bc00c8082128de1c160784b1a
Nathan <nathan.chan.zh@gmail.com>
2020-03-11 00:20:23 -0400

    Fixed task_sync test

--- src/controllers/task/task_list.test.ts
+++ src/controllers/task/task_list.test.ts
@@ -8,9 +8,14 @@ describe("Task List", ()=> {
         return await TestLoginActions.email_password();
     });
 
-    it('should retrieve an initial list of users when called', async () => {
-        let task_list = await task_sync(()=>{});
-        expect(task_list.tasks.length).toBeGreaterThan(0);
-        expect(task_list.groups.length).toBeGreaterThan(0);
+    it('should retrieve an initial list of users when called', async done => {
+        expect.assertions(2);
+        let task_list = await task_sync(()=>{
+            expect(task_list.tasks.length).toBeGreaterThan(0);
+            expect(task_list.groups.length).toBeGreaterThan(0);
+
+            done();
+        });
+        
     });
 });
