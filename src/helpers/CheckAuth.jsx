6ae0f2909e760b8e44b46c31e7017f9230966615
Nathan <nathan.chan.zh@gmail.com>
2020-04-16 18:33:53 -0400

    Fixed CheckAuth

--- src/helpers/CheckAuth.jsx
+++ src/helpers/CheckAuth.jsx
@@ -1,18 +1,18 @@
 import user from "../controllers/user/index";
 
-const CheckAuth = () => {
+const CheckAuth = async () => {
   const routeRedirect = window.document.createElement("ion-route-redirect");
 
   routeRedirect.setAttribute("from", "*");
   routeRedirect.setAttribute("to", "/login");
 
-  user.get_user().then(function (result) {
-    if (!result) {
-      let url = window.location.href.split("/");
-      url[3] = "login";
-      window.location.href = url.join("/");
-    }
-  });
+  console.log("ASD");
+  let result = await user.get_user();
+  if (!result) {
+    let url = window.location.href.split("/");
+    url[3] = "login";
+    window.location.href = url.join("/");
+  }
 };
 
 export default CheckAuth;
